a9e27be573f2d3e6e580c2e05ef2195d92d18536
sijo708 <sijo708@yahoo.com>
2024-07-01 14:28:27 -0400

    error fix

--- js/site.js
+++ js/site.js
@@ -7,29 +7,23 @@ function getValues() {
     buzzValue = parseInt(buzzValue);
     stopValue = parseInt(stopValue);
 
-    if (isNaN(fizzValue) && isNaN(buzzValue) && isNaN(stopValue)) {
-
-        Swal.fire(
-            {
-                icon: 'error',
-                title: 'Uh oh!',
-                text: 'Please enter a valid number for both the start and end values.',
-                backdrop: false
-            }
-        );
+    // Check if any of the values are invalid
+    if (isNaN(fizzValue) || isNaN(buzzValue) || isNaN(stopValue)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Uh oh!',
+            text: 'Please enter valid numbers for all values.',
+            backdrop: false
+        });
     } else if (stopValue > 5000) {
-
-        Swal.fire(
-            {
-                icon: 'error',
-                title: 'Uh oh!',
-                text: 'Please enter a valid number for both the start and end values.',
-                backdrop: false
-            }
-        );
+        Swal.fire({
+            icon: 'error',
+            title: 'Uh oh!',
+            text: 'Please enter a stop value less than or equal to 5000.',
+            backdrop: false
+        });
     } else {
         let fizzBuzzArray = generateFizzBuzz(fizzValue, buzzValue, stopValue);
-
         displayFizzBuzz(fizzBuzzArray);
     }
 }
@@ -38,18 +32,15 @@ function generateFizzBuzz(fizzValue, buzzValue, stopValue) {
     let generatedValues = [];
 
     for (let number = 1; number <= stopValue; number++) {
-        let value = '';
-
-        if (number % fizzValue == 0 && number % buzzValue == 0) {
+        if (number % fizzValue === 0 && number % buzzValue === 0) {
             generatedValues.push('FizzBuzz');
-        } else if (number % fizzValue == 0) {
-            generatedValues.push('Fizz');;
-        } else if (number % buzzValue == 0) {
+        } else if (number % fizzValue === 0) {
+            generatedValues.push('Fizz');
+        } else if (number % buzzValue === 0) {
             generatedValues.push('Buzz');
         } else {
-            generatedValues.push(number);;
+            generatedValues.push(number);
         }
-
     }
 
     return generatedValues;
@@ -57,14 +48,12 @@ function generateFizzBuzz(fizzValue, buzzValue, stopValue) {
 
 function displayFizzBuzz(generatedValues) {
     let resultsTable = document.getElementById('result');
-
     let resultHtml = '';
 
-    // for each number in values:
     for (let i = 0; i < generatedValues.length; i++) {
         let value = generatedValues[i];
 
-        if (i % 5 == 0) {
+        if (i % 5 === 0) {
             resultHtml += '<tr>';
         }
 
